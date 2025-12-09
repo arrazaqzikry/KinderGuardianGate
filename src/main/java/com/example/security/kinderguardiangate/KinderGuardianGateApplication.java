52a12fbb92ccf51d103b21debe04aab9cb11c457
arrazaq zikry <zikryarrazaq@gmail.com>
2025-12-09 23:18:32 +0800

    Initial commit

---
+++ src/main/java/com/example/security/kinderguardiangate/KinderGuardianGateApplication.java
@@ -0,0 +1,13 @@
+package com.example.security.kinderguardiangate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinderGuardianGateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KinderGuardianGateApplication.class, args);
+    }
+
+}
